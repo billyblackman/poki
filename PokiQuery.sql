e825e319d9062ad3317f7db6f4d49defde28c4f8
Billy Blackman <blackmanbilly@gmail.com>
2020-06-15 13:56:33 -0500

    Initial commit

---
+++ PokiQuery.sql
@@ -0,0 +1,22 @@
+--1
+--SELECT * FROM Grade;
+
+--2
+--SELECT * FROM Emotion;
+
+--3
+--SELECT COUNT(Id) FROM POEM;
+
+--4
+--SELECT NAME FROM Author
+--ORDER BY NAME;
+
+--5
+--SELECT NAME, GRADEID FROM AUTHOR
+--ORDER BY NAME;
+
+--6
+--SELECT author.NAME, author.GRADEID AS Grade, gender.Name AS Gender FROM AUTHOR
+--LEFT JOIN Gender gender ON gender.Id = author.GenderId
+--ORDER BY author.NAME;
+
